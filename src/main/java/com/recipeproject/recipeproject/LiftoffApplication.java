2dc96a362c64d4f172f5939bef972ddff0451a9e
Santiago <santi1795@hotmail.com>
2021-12-09 19:53:59 -0600

    Initial Commit

---
+++ src/main/java/com/recipeproject/recipeproject/LiftoffApplication.java
@@ -0,0 +1,13 @@
+package com.recipeproject.recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffApplication.class, args);
+	}
+
+}
