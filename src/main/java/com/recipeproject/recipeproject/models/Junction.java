5517a52992e66d92b0e2dcf9fe20bf070eb9aed4
Lucy134 <67610043+Lucy134@users.noreply.github.com>
2022-01-04 22:41:30 -0600

    test

--- src/main/java/com/recipeproject/recipeproject/models/Junction.java
+++ src/main/java/com/recipeproject/recipeproject/models/Junction.java
@@ -27,4 +27,47 @@ public class Junction {
     public Junction() {
     }
 
+    public int getJunctionID() {
+        return junctionID;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public Measurement getMeasurement() {
+        return measurement;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getPrepNotes() {
+        return prepNotes;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public void setMeasurement(Measurement measurement) {
+        this.measurement = measurement;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public void setPrepNotes(String prepNotes) {
+        this.prepNotes = prepNotes;
+    }
 }
